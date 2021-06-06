5cfa852187b500ad0fb28d673fa517fadbc6d2aa
uroojahamad <uroojahamad786@gmail.com>
2021-06-06 19:33:19 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,17 @@
+console.log('Arrow Function in JavaScript')
+
+// Normal Function 
+function addNumbers(firstNumber, secondNumber)
+{
+    return firstNumber + secondNumber;
+}
+
+console.log(addNumbers(5,5));
+
+// Arror Function
+const addNumber = (firstNum, secondNum) => {
+    return firstNum + secondNum;
+}
+
+console.log(addNumber(7,7));
+
