0a40a992ea66b5163df79e6dfe3dbdc504947857
uroojahamad <uroojahamad786@gmail.com>
2021-06-11 17:54:52 +0530

    created function to mark items as complete

--- TodoList/todolist.js
+++ TodoList/todolist.js
@@ -1,5 +1,6 @@
 const addItemToList = document.getElementById("addItemToList");
 const listItems = document.getElementById("listItem");
+const completedListItems = document.getElementById("completedListItem");
 
 //Event Listener to add items to list
 addItemToList.addEventListener("click", addItem);
@@ -23,7 +24,7 @@ function displayItemList(){
     let str = "";
     for (let items in itemArray) {
         str += `
-        <li class="list-group-item"><button class="text" onclick="strikeItems(this)">${itemArray[items]}</span>
+        <li class="list-group-item"><button class="text" onclick="completedItems(this)">${itemArray[items]}</span>
         <button class="deletedItem" onclick="deleteItem(this)"><i class="fa fa-trash"></i>Delete</button>
         </li> `;
     }
@@ -46,6 +47,32 @@ function deleteItem(e){
 
 }
 
-function strikeItems(e){
-    e.classList.toggle("strikeText");
-}
+// function strikeItems(e){
+//     e.classList.toggle("strikeText");
+// }
+
+//Completed List 
+
+const completedItemArray = [];
+function completedItems(e){
+   console.log(e);
+
+    const itemValue = e.innerText;
+    console.log(itemValue);
+    //Remove elements from an array
+    for (let items in itemArray) {
+        if(itemArray[items] == itemValue){
+            console.log("true");
+            itemArray.splice(items,1);
+            completedItemArray.unshift(itemValue);
+        }
+    }
+    // const completedItem = e.parentElement;
+    // completedItem.remove();
+    console.log(completedItemArray);
+    e.parentElement.remove();
+
+    completedItemList();
+
+    }
+
