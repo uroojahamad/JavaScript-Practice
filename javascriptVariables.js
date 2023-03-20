d72bd074e9e3e9097624b511928077108dd69b44
Urooj Ahmad <uroojahamad786@gmail.com>
2023-03-20 14:12:39 +0530

    added new scripts in advance.html

--- javascriptVariables.js
+++ javascriptVariables.js
@@ -3,21 +3,22 @@ console.log('Javascript Variables');
 //JavaScript Variables
 
 // 1. var (global scope/function scope)
-// var a = 23;
-// console.log('Value of a before if block', a)
+var a = 23;
+console.log('Value of a before if block', a)
 
-// if ( a == 23)
-// {
-//     a = 25;
-//     var b = 90;
-//     console.log('Value of a from if block', a);
-//     console.log('Value of b from if block', b);
-// }
+if ( a == 23)
+{
+    a = 25;
+    var b = 90;
+    console.log('Value of a from if block', a);
+    console.log('Value of b from if block', b);
+}
 
-// console.log('Value of a after the if block', a);
-// console.log('Value of b after if block', b);
+console.log('Value of a after the if block', a);
+console.log('Value of b after if block', b);
 
-//2. let (block level scope, can change value once assigned)
+// 2. let (block level scope, can change value once assigned)
+// console.log('Let Variable');
 // let a = 27;
 // console.log('Value of a before if block', a)
 
@@ -33,13 +34,14 @@ console.log('Javascript Variables');
 // console.log('Value of b after the if block', b);
 
 //3. const (block level scope, can't change the value once assigned)
+// console.log('Const Variable');
 // const a = 45;
 // console.log('Value of a before if block', a);
 
 // if ( a == 45)
 // {
 //     // a = 67;
-//     console.log('Value of a from if block', a);
+//     // console.log('Value of a from if block', a);
 
 //     const b = 78;
 //     console.log('Value of b from if block', b);
@@ -49,7 +51,7 @@ console.log('Javascript Variables');
 // console.log('Value of b after if block', b);
 
 //Template String / Literals
-const name = 'Urooj'
-const age = 23
+// const name = 'Urooj'
+// const age = 23
 
-console.log(`My name is ${name} and I am ${age} years old. `);
+// console.log(`My name is ${name} and I am ${age} years old. `);
