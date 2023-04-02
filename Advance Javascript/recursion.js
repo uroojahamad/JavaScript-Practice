772bab0390eacb8e9d4220ef3dc1d742f82f6d6b
Urooj Ahmad <uroojahamad786@gmail.com>
2023-04-02 16:55:55 +0530

    sum function using recursion

--- Advance Javascript/recursion.js
+++ Advance Javascript/recursion.js
@@ -1,34 +1,48 @@
 console.log("Recursion");
 
-const user ={
-    name : "Urooj",
-    address: {
-        personal: {
-            area: "Balaganj", 
-            city: "Lucknow"
-        },
-        office:{
-            city: "Gurugram",
-            area: {
-                landmark: "Info Tech City"
-            }
+//Problem : sum(1)(2)(3)(4)();
+
+let sum = (a) => {
+    return (b) =>{
+        if(b){
+            return sum(a + b);
         }
+        return a;
     }
 }
 
-let finalObj = {};
+console.log(sum(1)(2)(3)(4)());
 
-let recursive = (object, parent) => {
-    for (let key in object) {
-        if (typeof object[key] === "Object") {
-            recursive = (object[key], parent+"_"+key)
-        }
-        else{
-            finalObj[parent+"_"+key] = object[key];
-        }
-    }
-    return finalObj;
-}
 
-console.log(recursive(user, "user"))
+// const user ={
+//     name : "Urooj",
+//     address: {
+//         personal: {
+//             area: "Balaganj", 
+//             city: "Lucknow"
+//         },
+//         office:{
+//             city: "Gurugram",
+//             area: {
+//                 landmark: "Info Tech City"
+//             }
+//         }
+//     }
+// }
+
+// let finalObj = {};
+
+// let recursive = (object, parent) => {
+//     for (let key in object) {
+//         if (typeof object[key] === "Object") {
+//             recursive = (object[key], parent+"_"+key)
+//         }
+//         else{
+//             finalObj[parent+"_"+key] = object[key];
+//         }
+//     }
+//     return finalObj;
+// }
+
+// console.log(recursive(user, "user"))
 
