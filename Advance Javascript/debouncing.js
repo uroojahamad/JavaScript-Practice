55a6d10f0e53be4f18e2a4c0b0bef482106e7baf
Urooj Ahmad <uroojahamad786@gmail.com>
2024-01-17 00:04:35 +0530

    debouncing

--- Advance Javascript/debouncing.js
+++ Advance Javascript/debouncing.js
@@ -5,16 +5,30 @@ const getData = () =>{
     console.log(`Fetching Data....${counter++}`);
 }
 
+// const debounce = (func, delay) =>{
+//     let timer;
+//     return (arguments) =>{
+//         let context = this,
+//             args = arguments;
+//         clearTimeout(timer);
+//         timer = setTimeout(() => {
+//             func.apply(context, args)
+//         }, delay);
+//     }
+// }
+
+// 2nd approach
+
 const debounce = (func, delay) =>{
     let timer;
-    return (arguments) =>{
-        let context = this,
-            args = arguments;
-        clearTimeout(timer);
+    return (...args) =>{
+        if(timer)
+            clearTimeout(timer)
         timer = setTimeout(() => {
-            func.apply(context, args)
+            func(...args)
         }, delay);
     }
+
 }
 
 const search = debounce(getData, 300);
