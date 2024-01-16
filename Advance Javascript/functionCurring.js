23cc2f4a3b689ff6dde0a80117c9954f2611a055
Urooj Ahmad <uroojahamad786@gmail.com>
2024-01-17 00:05:05 +0530

    functiona curring

--- Advance Javascript/functionCurring.js
+++ Advance Javascript/functionCurring.js
@@ -7,6 +7,41 @@ console.log("Function Curring");
         ii. using closure
 */
 
+const addNumber = (a, b, c) => {
+    return a + b + c;
+}
+
+console.log(addNumber(2, 5, 3));
+console.log(addNumber(2, 5));
+
+
+const curryAdd = (a) => {
+    return (b) => {
+        return a + b;
+    }
+}
+
+const res = curryAdd(2);
+console.log(res(3))
+// console.log(curryAdd(2)(3))
+
+
+const mail = (to) => {
+    return (subject) => {
+        return (body) => {
+            return `${to} ${subject} ${body}`
+        }
+    }
+}
+
+console.log(mail("test@test.com")("Test Mail")("This is test mail"))
+
+
+
+
+
+
+
 //Using bind method
 // const multiply = (x, y) => {
 //     console.log(x * y);
@@ -19,37 +54,37 @@ console.log("Function Curring");
 // multiplyByThree(5);
 
 //using Closures
-const multiply = (x) =>{
-    return (y) => {
-        console.log(x * y)
-    }
-}
-const multiplyByTwo = multiply(2);
-multiplyByTwo(5);
+// const multiply = (x) => {
+//     return (y) => {
+//         console.log(x * y)
+//     }
+// }
+// const multiplyByTwo = multiply(2);
+// multiplyByTwo(5);
 
-const multiplyByThree = multiply(3);
-multiplyByThree(5);
+// const multiplyByThree = multiply(3);
+// multiplyByThree(5);
 
 // const add = (a, b) =>{
 //     console.log(a + b);
 // }
 // add(5, 5);
 
-const add = (x) => {
-    return (y) =>{
-        console.log(x + y)
-    }
-}
+// const add = (x) => {
+//     return (y) => {
+//         console.log(x + y)
+//     }
+// }
 
 // one liner for above : const add = (x) => (y) => console.log(x + y)
-add(6)(6);
+// add(6)(6);
 
-const sum = (a) =>{
-    return (b) =>{
-        return (c) =>{
-            return a + b + c
-        }
-    }
-}
+// const sum = (a) => {
+//     return (b) => {
+//         return (c) => {
+//             return a + b + c
+//         }
+//     }
+// }
 
-console.log(sum(1)(2)(3));
+// console.log(sum(1)(2)(3));
