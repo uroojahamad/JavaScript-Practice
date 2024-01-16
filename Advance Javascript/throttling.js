f233edfc2b5f4ddd2d40ce8aa588aa8a80d34367
Urooj Ahmad <uroojahamad786@gmail.com>
2024-01-17 00:05:25 +0530

    throttling

--- Advance Javascript/throttling.js
+++ Advance Javascript/throttling.js
@@ -1,21 +1,35 @@
 console.log("Throttling");
 
 const expensive = () =>{
-    console.log("Expensive Function call")
+    let count = 0 ;
+    console.log(`Expensive Function call ${count++}`)
 }
 
-const throttle = (func, limit) => {
-    let flag = true;
-    return function(){
-        let context = this;
-        let args = arguments;
-        if(flag){
-            func.apply(context, args);
-            flag = false;
-            setTimeout(() => {
-                flag = true  
-            }, limit);
-        }
+// const throttle = (func, limit) => {
+//     let flag = true;
+//     let count = 0;
+//     return function(){
+//         let context = this;
+//         let args = arguments;
+//         if(flag){
+//             func.apply(context, args);
+//             flag = false;
+//             setTimeout(() => {
+//                 console.log(count++)
+//                 flag = true  
+//             }, limit);
+//         }
+//     }
+// }
+
+const throttle = (func, delay) =>{
+    let last = 0;
+    return (...args) =>{
+        let now = new Date().getTime();
+        console.log(now)
+        if(now - last < delay) return;
+        last = now
+        return func(...args)
     }
 }
 const betterExperience = throttle(expensive, 2000)
